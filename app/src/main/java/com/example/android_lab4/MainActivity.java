6ae9e914b29205a39a3ab070a2383dac55beabb1
darlinxx <daria061296@wp.pl>
2020-06-12 21:30:49 +0200

    commit2

--- app/src/main/java/com/example/android_lab4/MainActivity.java
+++ app/src/main/java/com/example/android_lab4/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int a;
+        double b=88.8;
     }
 }
