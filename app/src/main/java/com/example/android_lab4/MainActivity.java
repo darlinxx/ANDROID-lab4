48775925c4a805f0f502a5b1a4322d9a25bd0c5f
darlinxx <daria061296@wp.pl>
2020-06-12 21:50:52 +0200

    commit3

--- app/src/main/java/com/example/android_lab4/MainActivity.java
+++ app/src/main/java/com/example/android_lab4/MainActivity.java
@@ -2,15 +2,55 @@ package com.example.android_lab4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> target;
+    private ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int a;
-        double b=88.8;
+
+        String[] values = new String[] { "Pies", "Kot", "Koń", "Gołąb", "Kruk", "Dzik", "Karp","Osioł", "Chomik", "Mysz", "Jeż", "Kraluch" };
+
+        this.target = new ArrayList<String>();
+        this.target.addAll(Arrays.asList(values));
+
+        this.adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, this.target);
+
+        ListView listview = (ListView) findViewById(R.id.listView );
+        listview.setAdapter(this.adapter);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    public void nowyWpis(MenuItem mi)
+    {
+        Intent intencja = new Intent(this,
+                DodajWpis.class);
+        startActivityForResult(intencja, 1);
+    }
+
+
+
 }
+
+
