af347bebb78a2d9e3dca220faa2f6776018660cb
darlinxx <daria061296@wp.pl>
2020-06-12 21:54:31 +0200

    commit4

--- app/src/main/java/com/example/android_lab4/MainActivity.java
+++ app/src/main/java/com/example/android_lab4/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android_lab4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -44,11 +45,22 @@ public class MainActivity extends AppCompatActivity {
 
     public void nowyWpis(MenuItem mi)
     {
-        Intent intencja = new Intent(this,
-                DodajWpis.class);
+        Intent intencja = new Intent(this,DodajWpis.class);
         startActivityForResult(intencja, 1);
     }
 
+    @SuppressLint("MissingSuperCall")
+    @Override
+    protected void onActivityResult(
+            int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            String nowy = (String) extras.get("wpis");
+            target.add(nowy);
+            adapter.notifyDataSetChanged();
+        }
+    }
 
 
 }
